b8f14d09b577565007927353f6612cf747daecad
emipere <151584933+emipere@users.noreply.github.com>
2024-04-25 14:51:30 -0300

    trabajo final

---
+++ carro.js
@@ -0,0 +1,91 @@
+let cartsotarge = localStorage.getItem("fichaArticulo")
+cartsotarge = JSON.parse(cartsotarge) 
+
+let cartContaainer = document.getElementById ("seccion-carro")
+
+function renderCarrito(cartItems) {
+
+    let total = 0;
+
+    cartContaainer.innerHTML = '';
+    
+    cartItems.forEach (articulo => {
+        const cart = document.createElement ("div")
+        cart.className = 'card mt-2'
+        cart.innerHTML = `<div class="card-body">
+                            <img src="${articulo.articulo.img}"width=150>
+                            <h3 class="card-title">${articulo.articulo.nombre}</h3>
+                            <h4 class="card-text">${articulo.cantidad} x $${articulo.articulo.precio}</h4>
+                            <button class="btn btn-primary btn-agregar" id="${articulo.articulo.id}">+</button>                            
+                            <button class="btn btn-primary btn-disminuir" id="${articulo.articulo.id}">-</button>
+                            <button class="btn btn-danger btn-eliminar" id="${articulo.articulo.id}">Eliminar</button>
+                            </div>`
+         cartContaainer.appendChild(cart)                
+         total += articulo.articulo.precio * parseInt(articulo.cantidad)
+    });
+
+    const totalcontainer = document.createElement ("div")
+    totalcontainer.innerHTML = `<h3>Total $${total}</h3>`;
+    cartContaainer.appendChild(totalcontainer)  
+    
+    configButtons(cartsotarge)       
+
+}
+
+function configButtons(cartItems) {
+
+    let agregar = document.querySelectorAll('.btn-agregar');
+    let disminuir = document.querySelectorAll('.btn-disminuir');
+    let eliminar = document.querySelectorAll('.btn-eliminar');
+
+    agregar.forEach(button => {
+
+        button.onclick = (art) => {
+            const artid = art.currentTarget.id;
+            const articuloSeleccionado = cartItems.find(articulo => articulo.articulo.id == artid)
+
+            if (articuloSeleccionado) {
+                articuloSeleccionado.cantidad++;
+                localStorage.setItem("fichaArticulo", JSON.stringify(cartItems))
+                renderCarrito(cartsotarge)
+            }
+        }
+
+    });
+
+    disminuir.forEach(button => {
+
+        button.onclick = (art) => {
+            const artid = art.currentTarget.id;
+            const articuloSeleccionado = cartItems.find(articulo => articulo.articulo.id == artid)
+
+            if (articuloSeleccionado) {
+                articuloSeleccionado.cantidad--;
+                if (articuloSeleccionado.cantidad == 0) {
+                    articuloSeleccionado.cantidad = 1;
+                }
+                localStorage.setItem("fichaArticulo", JSON.stringify(cartItems))
+                renderCarrito(cartsotarge)
+            }
+        }
+
+    });
+
+    eliminar.forEach(button => {
+
+        button.onclick = (art) => {
+            const artid = art.currentTarget.id;
+            const idx = cartItems.findIndex(articulo => articulo.articulo.id == artid)
+
+            if (idx >= 0) {
+                cartItems.splice(idx, 1);
+                localStorage.setItem("fichaArticulo", JSON.stringify(cartItems))
+                renderCarrito(cartsotarge)
+            }
+        }
+
+    });
+
+}
+
+renderCarrito(cartsotarge)
---
+++ main.js
@@ -0,0 +1,133 @@
+
+
+let fichaArticulo
+let fichaDeArticuloLS = localStorage.getItem("fichaArticulo")
+if (fichaDeArticuloLS) {
+    fichaArticulo = JSON.parse(fichaDeArticuloLS)
+} else {
+    fichaArticulo = []
+}
+
+
+
+let articulos = []
+
+fetch("../db/data.json")
+    .then(response => response.json())
+    .then(data => { articulos = data; renderProducto() })
+
+
+
+function renderProducto() {
+    const listaDeArticulos = document.getElementById("articulos")
+    articulos.forEach((articulo) => {
+        const card = document.createElement("div")
+        card.innerHTML = `<nav class="navbar bg-body-tertiary">
+                       <div class="container-fluid"></div>
+                      </nav>
+                      <div class="container p-3">
+                      <div class="row">
+                      <section id="seccion-carro" class="col-12">
+                      <h3>Nombre: ${articulo.articulo.nombre}</h3>
+                      <h4>Precio: $${articulo.articulo.precio}</h4>     
+                      <img src="${articulo.articulo.img}" width=150>                 
+                      <button class="boton-aumentar" id="${articulo.articulo.id}">+</button>
+                      <button class="cantidad" id="${articulo.articulo.id}">${articulo.cantidad}</button>
+                      <button class="boton-disminuir" id="${articulo.articulo.id}">-</button>                      
+                      <button class="articuloAgregar" id="${articulo.articulo.id}">Agregar al Carro</button>`
+
+        listaDeArticulos.appendChild(card)
+    })
+
+}
+
+botonAgregarAlCarro()
+
+function botonAgregarAlCarro() {
+    let addbutton = document.querySelectorAll(".articuloAgregar");
+    console.log(addbutton);
+    addbutton.forEach(button => {
+        button.onclick = (art) => {
+
+            const artid = art.currentTarget.id
+            const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid)
+
+            const articuloCarrito = fichaArticulo.find(articulo => articulo.articulo.id == artid);
+
+            if (!articuloCarrito) {
+                fichaArticulo.push(articuloSeleccionado)
+            } else {
+                articuloCarrito.cantidad += articuloSeleccionado.cantidad;
+            }
+
+            console.log(fichaArticulo)
+            localStorage.setItem("fichaArticulo", JSON.stringify(fichaArticulo))
+
+            Toastify({
+                text: "El Producto fue añadido al carrito Exitosamente!",
+                duration: 1500,
+                destination: "",
+                newWindow: false,
+                close: false,
+                gravity: "top", // `top` or `bottom`
+                position: "center", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                },
+                onClick: function () { } // Callback after click
+            }).showToast();
+
+
+        }
+    })
+}
+
+
+// contador
+
+let cantidad = document.querySelectorAll(".cantidad")
+let aumentar = document.querySelectorAll(".boton-aumentar")
+let disminuir = document.querySelectorAll(".boton-disminuir")
+let contador = 0
+
+aumentar.forEach(button => {
+
+    button.onclick = (art) => {
+
+        const artid = art.currentTarget.id;
+        const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid);
+        articuloSeleccionado.cantidad++;
+
+        cantidad.forEach(element => {
+            if (element.id == artid) {
+                element.innerHTML = articuloSeleccionado.cantidad;
+            }
+        });
+
+    };
+
+});
+
+disminuir.forEach(button => {
+
+    button.onclick = (art) => {
+
+        const artid = art.currentTarget.id;
+        const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid);
+        articuloSeleccionado.cantidad--;
+
+        if (articuloSeleccionado.cantidad < 1) {
+            articuloSeleccionado.cantidad = 1;
+        }
+
+        cantidad.forEach(element => {
+            if (element.id == artid) {
+                element.innerHTML = articuloSeleccionado.cantidad;
+            }
+        });
+
+    };
+
+});
+
