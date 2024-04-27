db86b9ec2ca0af5d04d5d3c8bdf8b7538827d203
emipere <emipereiro@gmail.com>
2024-04-27 17:13:26 -0300

    falta solo tema del json

--- main.js
+++ main.js
@@ -59,53 +59,6 @@ if(fichaDeArticuloLS){
 let listaDeArticulos = document.getElementById("articulos")
 
 
-// --ESTA ES UNA MANERA DE AGARRAR EL JSON PERO NO ME DEJA SUMAR NI NAADA CEN EL CARRO--
-// async function renderProducto() {
-//     try {
-//       ---TENGO LA RESP DEL JSON--
-//       const response = await fetch('../db/data.json');
-  
-//       ---VERIFICO SI LA RESPUESTA FUE OK---
-//       if (!response.ok) {
-//         throw new Error(`Error al cargar datos JSON: ${response.status}`);
-//       }
-  
-//       --CONVIERTO LA RESP A ORMATO JSON
-//       const data = await response.json();
-  
-//       -- LE DOY CURSO AL JSON--
-//       data.forEach((articulo) => {
-//         const card = document.createElement("div");
-//         card.innerHTML = `
-//           <nav class="navbar bg-body-tertiary">
-//             <div class="container-fluid"></div>
-//           </nav>
-//           <div class="container p-3">
-//             <div class="row">
-//               <section id="seccion-carro" class="col-12">
-//               <h3>Nombre: ${articulo.articulo.nombre}</h3>
-//                       <h4>Precio: $${articulo.articulo.precio}</h4>     
-//                       <img src="${articulo.articulo.img}" width=150>                 
-//                       <button class="boton-aumentar" id="${articulo.articulo.id}">+</button>
-//                      <button class="cantidad" id="${articulo.articulo.id}">${articulo.cantidad}</button>
-//                      <button class="boton-disminuir" id="${articulo.articulo.id}">-</button>                      
-//                       <button class="articuloAgregar" id="${articulo.articulo.id}">Agregar al Carro</button>  
-//               </section>
-//             </div>
-//           </div>
-//         `;
-  
-//         listaDeArticulos.appendChild(card);
-//       });
-//     } catch (error) {
-//       console.error("Error al cargar datos JSON:", error);
-//       // Aquí puedes agregar código para manejar el error de forma específica, como mostrar un mensaje al usuario o registrar el error en un servicio de logs.
-//     } finally {
-//       console.log("Finalizando la función renderProducto");
-//     }
-//   }
-
-
 function renderProducto(articulos) {
  articulos.forEach((articulo) => {
     const card = document.createElement("div")
