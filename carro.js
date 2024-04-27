d31702e098dc30fb6b5c2e2652540027e21c89cb
emipere <emipereiro@gmail.com>
2024-04-27 15:46:39 -0300

    trabaje botones en el carro, me falta el json

--- carro.js
+++ carro.js
@@ -20,35 +20,41 @@ function renderCarrito(cartItems) {
                             <button class="btn btn-primary btn-disminuir" id="${articulo.articulo.id}">-</button>
                             <button class="btn btn-danger btn-eliminar" id="${articulo.articulo.id}">Eliminar</button>
                             </div>`
-         cartContaainer.appendChild(cart)                
+         cartContaainer.appendChild(cart)  
+
+                   
          total += articulo.articulo.precio * parseInt(articulo.cantidad)
     });
 
-    let exit = document.getElementById ("exit")
+        let exit = document.getElementById ("exit")
         const fin = document.createElement ("div")
         fin.className = 'card mt-2'
         if (total >0) {
         fin.innerHTML = `<div>
                             <button class="btn btn-danger btn-vaciarCarro">Vaciar Carro</button>
                             </div>`
-                            // no se como vaciar y a que cosa referenciarla
+                           
         cartContaainer.appendChild(fin) 
         };
+
+
 
         let pagar = document.getElementById ("pagar")
         if (total >0){
         const remito = document.createElement ("div")
         remito.className = 'card mt-2'
         remito.innerHTML = `<div>
-                            <button class="btn btn-primary"> Emitir Orden de Pago </button>
+                            <button class="btn btn-primary btn-emitirOrdenDePago"> Emitir Orden de Pago </button>
                             </div>`
 
-         cartContaainer.appendChild(remito)  
+         cartContaainer.appendChild(remito);
+         
+          
         };
-
+        
 
     const totalcontainer = document.createElement ("div")
-    totalcontainer.innerHTML = `<h3>Total $${total}</h3>`;
+    totalcontainer.innerHTML = `<h2>Total $${total}</h2>`;
     cartContaainer.appendChild(totalcontainer)  
     
     configButtons(cartsotarge)       
@@ -60,6 +66,8 @@ function configButtons(cartItems) {
     let agregar = document.querySelectorAll('.btn-agregar');
     let disminuir = document.querySelectorAll('.btn-disminuir');
     let eliminar = document.querySelectorAll('.btn-eliminar');
+    let vaciarCarro = document.querySelectorAll('.btn-vaciarCarro');
+    let emitirOrdenDePago= document.querySelectorAll('.btn-emitirOrdenDePago')
 
     agregar.forEach(button => {
 
@@ -104,6 +112,7 @@ function configButtons(cartItems) {
                 cartItems.splice(idx, 1);
                 localStorage.setItem("fichaArticulo", JSON.stringify(cartItems))
                 renderCarrito(cartsotarge)
+
                 Toastify({
                     text: "El Producto fue eliminado del carrito Exitosamente!",
                     duration: 1500,
@@ -123,21 +132,50 @@ function configButtons(cartItems) {
 
     });
 
+
+vaciarCarro.forEach(button => {
+    button.onclick = async () => { 
+      const confirmacion = await Swal.fire({
+        title: "¿Estás seguro de vaciar el carrito?",
+        text: "¡Esta acción no se puede deshacer!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, vaciar',
+        cancelButtonText: 'Cancelar'
+      });
+
+      if (confirmacion.isConfirmed) {
+        cartsotarge = []; 
+        localStorage.setItem("fichaArticulo", JSON.stringify(cartsotarge));
+        renderCarrito(cartsotarge); 
+        Swal.fire('Carrito Vaciado!', '', 'success'); 
+      }
+    }
+  })
+  
+  
+
+
+emitirOrdenDePago.forEach(button => {
+    button.onclick = async () => {
+
+        const { value: email } = await Swal.fire({
+            title: "Gracias por su compra. " + "Le enviaremos la orden para su posterior pago.",
+            input: "email",
+            inputLabel: "Por favor complete su mail en el siguiente recuadro",
+            inputPlaceholder: "Enter your email address"
+          });
+          if (email) {
+            Swal.fire(`Por favor dirijase al Centro de Pagos con la orden que la fue enviada al correo : ${email}`);
+          }
 }
+})
+
+ }
 
 renderCarrito(cartsotarge)
 
-// NO SUPE APLICARLE LIBRERIA AL BOTON VACIAR  Y AL BOTON EMITIR PAGO NO VACIAR EL CARRO
-
-// let vaciarCarro = document.querySelectorAll('.btn-vaciarCarro');
-
-// button.onclick = (vaciarCarro) => {
-//     Swal.fire({
-//         title: "The Internet?",
-//         text: "That thing is still around?",
-//         icon: "question"
-//       })
-//     }
 
 
-// vaciarCarro ()
--- main.js
+++ main.js
@@ -1,28 +1,115 @@
+const articulos = [  
+    {
+        articulo: {
+            id : 1,
+            nombre : "Gorra",
+            precio : 2000,       
+            img: href = "img/gorra.jpg"   
+        },
+        cantidad: 1
+    },
+    {
+        articulo: {
+            id : 2,
+            nombre : "Campera",
+            precio : 12000,
+            img: href = "img/campera.jpg" 
+        },
+        cantidad: 1
+    },
+    {
+        articulo: {
+            id : 3,
+            nombre : "Remera",
+            precio : 8000,
+            img: href = "img/remera.jpg" 
+        },
+        cantidad: 1
+    },
+    {
+        articulo: {
+            id : 4,
+            nombre : "Short",
+            precio : 3000,
+            img: href = "img/short.jpg" 
+        },
+        cantidad: 1
+    },
+    {
+        articulo: {
+            id : 5,
+            nombre : "Medias",
+            precio : 1500,
+            img: href = "img/medias.jpg" 
+        },
+        cantidad: 1
+    },
+];
 
 
-let fichaArticulo
+let fichaArticulo 
 let fichaDeArticuloLS = localStorage.getItem("fichaArticulo")
-if (fichaDeArticuloLS) {
+if(fichaDeArticuloLS){
     fichaArticulo = JSON.parse(fichaDeArticuloLS)
-} else {
+}else {
     fichaArticulo = []
 }
 
 
-
-let articulos = []
-
-fetch("../db/data.json")
-    .then(response => response.json())
-    .then(data => { articulos = data; renderProducto() })
+let listaDeArticulos = document.getElementById("articulos")
 
 
+// --ESTA ES UNA MANERA DE AGARRAR EL JSON PERO NO ME DEJA SUMAR NI NAADA CEN EL CARRO--
+// async function renderProducto() {
+//     try {
+//       ---TENGO LA RESP DEL JSON--
+//       const response = await fetch('../db/data.json');
+  
+//       ---VERIFICO SI LA RESPUESTA FUE OK---
+//       if (!response.ok) {
+//         throw new Error(`Error al cargar datos JSON: ${response.status}`);
+//       }
+  
+//       --CONVIERTO LA RESP A ORMATO JSON
+//       const data = await response.json();
+  
+//       -- LE DOY CURSO AL JSON--
+//       data.forEach((articulo) => {
+//         const card = document.createElement("div");
+//         card.innerHTML = `
+//           <nav class="navbar bg-body-tertiary">
+//             <div class="container-fluid"></div>
+//           </nav>
+//           <div class="container p-3">
+//             <div class="row">
+//               <section id="seccion-carro" class="col-12">
+//               <h3>Nombre: ${articulo.articulo.nombre}</h3>
+//                       <h4>Precio: $${articulo.articulo.precio}</h4>     
+//                       <img src="${articulo.articulo.img}" width=150>                 
+//                       <button class="boton-aumentar" id="${articulo.articulo.id}">+</button>
+//                      <button class="cantidad" id="${articulo.articulo.id}">${articulo.cantidad}</button>
+//                      <button class="boton-disminuir" id="${articulo.articulo.id}">-</button>                      
+//                       <button class="articuloAgregar" id="${articulo.articulo.id}">Agregar al Carro</button>  
+//               </section>
+//             </div>
+//           </div>
+//         `;
+  
+//         listaDeArticulos.appendChild(card);
+//       });
+//     } catch (error) {
+//       console.error("Error al cargar datos JSON:", error);
+//       // Aquí puedes agregar código para manejar el error de forma específica, como mostrar un mensaje al usuario o registrar el error en un servicio de logs.
+//     } finally {
+//       console.log("Finalizando la función renderProducto");
+//     }
+//   }
 
-function renderProducto() {
-    const listaDeArticulos = document.getElementById("articulos")
-    articulos.forEach((articulo) => {
-        const card = document.createElement("div")
-        card.innerHTML = `<nav class="navbar bg-body-tertiary">
+
+function renderProducto(articulos) {
+ articulos.forEach((articulo) => {
+    const card = document.createElement("div")
+    card.innerHTML = `<nav class="navbar bg-body-tertiary">
                        <div class="container-fluid"></div>
                       </nav>
                       <div class="container p-3">
@@ -34,54 +121,62 @@ function renderProducto() {
                       <button class="boton-aumentar" id="${articulo.articulo.id}">+</button>
                       <button class="cantidad" id="${articulo.articulo.id}">${articulo.cantidad}</button>
                       <button class="boton-disminuir" id="${articulo.articulo.id}">-</button>                      
-                      <button class="articuloAgregar" id="${articulo.articulo.id}">Agregar al Carro</button>`
+                      <button class="articuloAgregar" id="${articulo.articulo.id}">Agregar al Carro</button>` 
+    
+    listaDeArticulos.appendChild (card)                      
+})
 
-        listaDeArticulos.appendChild(card)
-    })
+}                  
 
-}
+renderProducto(articulos)
 
 botonAgregarAlCarro()
 
-function botonAgregarAlCarro() {
+function botonAgregarAlCarro () {
     let addbutton = document.querySelectorAll(".articuloAgregar");
     console.log(addbutton);
-    addbutton.forEach(button => {
-        button.onclick = (art) => {
-
-            const artid = art.currentTarget.id
-            const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid)
-
-            const articuloCarrito = fichaArticulo.find(articulo => articulo.articulo.id == artid);
-
-            if (!articuloCarrito) {
-                fichaArticulo.push(articuloSeleccionado)
-            } else {
-                articuloCarrito.cantidad += articuloSeleccionado.cantidad;
-            }
-
-            console.log(fichaArticulo)
-            localStorage.setItem("fichaArticulo", JSON.stringify(fichaArticulo))
-
-            Toastify({
-                text: "El Producto fue añadido al carrito Exitosamente!",
-                duration: 1500,
-                destination: "",
-                newWindow: false,
-                close: false,
-                gravity: "top", // `top` or `bottom`
-                position: "center", // `left`, `center` or `right`
-                stopOnFocus: true, // Prevents dismissing of toast on hover
-                style: {
-                    background: "linear-gradient(to right, #00b09b, #96c93d)",
-                },
-                onClick: function () { } // Callback after click
-            }).showToast();
-
-
+    addbutton.forEach (button => {
+      button.onclick = (art) => {
+        try {
+          const artid = art.currentTarget.id;
+          const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid);
+  
+          const articuloCarrito = fichaArticulo.find(articulo => articulo.articulo.id == artid);
+  
+          if (!articuloCarrito) {
+            fichaArticulo.push(articuloSeleccionado);
+          } else {
+            articuloCarrito.cantidad += articuloSeleccionado.cantidad;
+          }
+  
+          console.log(fichaArticulo);
+          localStorage.setItem("fichaArticulo", JSON.stringify(fichaArticulo));
+  
+          Toastify({
+            text: "El Producto fue añadido al carrito Exitosamente!",
+            duration: 1500,
+            destination: "",
+            newWindow: false,
+            close: false,
+            gravity: "top", 
+            position: "center", 
+            stopOnFocus: true, 
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            onClick: function(){}
+          }).showToast();
+  
+        } catch (error) {
+          console.error("Error al agregar producto al carrito:", error);
+          
+        } finally {
+          
+          console.log("Finalizando la función botonAgregarAlCarro");
         }
+      }
     })
-}
+  }
 
 
 // contador
@@ -94,7 +189,7 @@ let contador = 0
 aumentar.forEach(button => {
 
     button.onclick = (art) => {
-
+        
         const artid = art.currentTarget.id;
         const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid);
         articuloSeleccionado.cantidad++;
