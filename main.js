f938f9034dfdf692f7024e1e5f36067d9495264b
emipere <emipereiro@gmail.com>
2024-04-29 21:51:48 -0300

    modifique el tiempo del cargando

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ fetch("./db/data.json").then((resp) => {
   setTimeout(() => {
     Swal.close();
     resp.json().then((data) => renderProducto(data));
-  }, 2000);
+  }, 1000);
 });
 
 function botonAgregarAlCarro() {
