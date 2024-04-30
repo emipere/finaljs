1fa01dfcf3cd130810bc8b54138b9c8cfe06db04
emipere <emipereiro@gmail.com>
2024-04-29 21:45:20 -0300

    ordene el codigo

--- carro.js
+++ carro.js
@@ -1,181 +1,170 @@
-let cartsotarge = localStorage.getItem("fichaArticulo")
-cartsotarge = JSON.parse(cartsotarge) 
+let cartsotarge = localStorage.getItem("fichaArticulo");
+cartsotarge = JSON.parse(cartsotarge);
 
-let cartContaainer = document.getElementById ("seccion-carro")
+let cartContaainer = document.getElementById("seccion-carro");
 
 function renderCarrito(cartItems) {
+  let total = 0;
 
-    let total = 0;
+  cartContaainer.innerHTML = "";
 
-    cartContaainer.innerHTML = '';
-    
-    cartItems.forEach (articulo => {
-        const cart = document.createElement ("div")
-        cart.className = 'card mt-2'
-        cart.innerHTML = `<div class="card-body">
+  cartItems.forEach((articulo) => {
+    const cart = document.createElement("div");
+    cart.className = "card mt-2";
+    cart.innerHTML = `<div class="card-body">
                             <img src="${articulo.articulo.img}"width=150>
                             <h3 class="card-title">${articulo.articulo.nombre}</h3>
                             <h4 class="card-text">${articulo.cantidad} x $${articulo.articulo.precio}</h4>
                             <button class="btn btn-primary btn-agregar" id="${articulo.articulo.id}">+</button>                            
                             <button class="btn btn-primary btn-disminuir" id="${articulo.articulo.id}">-</button>
                             <button class="btn btn-danger btn-eliminar" id="${articulo.articulo.id}">Eliminar</button>
-                            </div>`
-         cartContaainer.appendChild(cart)  
+                            </div>`;
+    cartContaainer.appendChild(cart);
 
-                   
-         total += articulo.articulo.precio * parseInt(articulo.cantidad)
-    });
+    total += articulo.articulo.precio * parseInt(articulo.cantidad);
+  });
 
-        let exit = document.getElementById ("exit")
-        const fin = document.createElement ("div")
-        fin.className = 'card mt-2'
-        if (total >0) {
-        fin.innerHTML = `<div>
+  let exit = document.getElementById("exit");
+  const fin = document.createElement("div");
+  fin.className = "card mt-2";
+  if (total > 0) {
+    fin.innerHTML = `<div>
                             <button class="btn btn-danger btn-vaciarCarro">Vaciar Carro</button>
-                            </div>`
-                           
-        cartContaainer.appendChild(fin) 
-        };
+                            </div>`;
 
+    cartContaainer.appendChild(fin);
+  }
 
-
-        let pagar = document.getElementById ("pagar")
-        if (total >0){
-        const remito = document.createElement ("div")
-        remito.className = 'card mt-2'
-        remito.innerHTML = `<div>
+  let pagar = document.getElementById("pagar");
+  if (total > 0) {
+    const remito = document.createElement("div");
+    remito.className = "card mt-2";
+    remito.innerHTML = `<div>
                             <button class="btn btn-primary btn-emitirOrdenDePago"> Emitir Orden de Pago </button>
-                            </div>`
+                            </div>`;
 
-         cartContaainer.appendChild(remito);
-         
-          
-        };
-        
+    cartContaainer.appendChild(remito);
+  }
 
-    const totalcontainer = document.createElement ("div")
-    totalcontainer.innerHTML = `<h2>Total $${total}</h2>`;
-    cartContaainer.appendChild(totalcontainer)  
-    
-    configButtons(cartsotarge)       
+  const totalcontainer = document.createElement("div");
+  totalcontainer.innerHTML = `<h2>Total $${total}</h2>`;
+  cartContaainer.appendChild(totalcontainer);
 
+  configButtons(cartsotarge);
 }
 
 function configButtons(cartItems) {
+  let agregar = document.querySelectorAll(".btn-agregar");
+  let disminuir = document.querySelectorAll(".btn-disminuir");
+  let eliminar = document.querySelectorAll(".btn-eliminar");
+  let vaciarCarro = document.querySelectorAll(".btn-vaciarCarro");
+  let emitirOrdenDePago = document.querySelectorAll(".btn-emitirOrdenDePago");
 
-    let agregar = document.querySelectorAll('.btn-agregar');
-    let disminuir = document.querySelectorAll('.btn-disminuir');
-    let eliminar = document.querySelectorAll('.btn-eliminar');
-    let vaciarCarro = document.querySelectorAll('.btn-vaciarCarro');
-    let emitirOrdenDePago= document.querySelectorAll('.btn-emitirOrdenDePago')
+  agregar.forEach((button) => {
+    button.onclick = (art) => {
+      const artid = art.currentTarget.id;
+      const articuloSeleccionado = cartItems.find(
+        (articulo) => articulo.articulo.id == artid
+      );
 
-    agregar.forEach(button => {
+      if (articuloSeleccionado) {
+        articuloSeleccionado.cantidad++;
+        localStorage.setItem("fichaArticulo", JSON.stringify(cartItems));
+        renderCarrito(cartsotarge);
+      }
+    };
+  });
 
-        button.onclick = (art) => {
-            const artid = art.currentTarget.id;
-            const articuloSeleccionado = cartItems.find(articulo => articulo.articulo.id == artid)
+  disminuir.forEach((button) => {
+    button.onclick = (art) => {
+      const artid = art.currentTarget.id;
+      const articuloSeleccionado = cartItems.find(
+        (articulo) => articulo.articulo.id == artid
+      );
 
-            if (articuloSeleccionado) {
-                articuloSeleccionado.cantidad++;
-                localStorage.setItem("fichaArticulo", JSON.stringify(cartItems))
-                renderCarrito(cartsotarge)
-            }
+      if (articuloSeleccionado) {
+        articuloSeleccionado.cantidad--;
+        if (articuloSeleccionado.cantidad == 0) {
+          articuloSeleccionado.cantidad = 1;
         }
+        localStorage.setItem("fichaArticulo", JSON.stringify(cartItems));
+        renderCarrito(cartsotarge);
+      }
+    };
+  });
 
-    });
+  eliminar.forEach((button) => {
+    button.onclick = (art) => {
+      const artid = art.currentTarget.id;
+      const idx = cartItems.findIndex(
+        (articulo) => articulo.articulo.id == artid
+      );
 
-    disminuir.forEach(button => {
+      if (idx >= 0) {
+        cartItems.splice(idx, 1);
+        localStorage.setItem("fichaArticulo", JSON.stringify(cartItems));
+        renderCarrito(cartsotarge);
 
-        button.onclick = (art) => {
-            const artid = art.currentTarget.id;
-            const articuloSeleccionado = cartItems.find(articulo => articulo.articulo.id == artid)
+        Toastify({
+          text: "El Producto fue eliminado del carrito Exitosamente!",
+          duration: 1500,
+          destination: "",
+          newWindow: false,
+          close: false,
+          gravity: "top",
+          position: "center",
+          stopOnFocus: true,
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          onClick: function () {},
+        }).showToast();
+      }
+    };
+  });
 
-            if (articuloSeleccionado) {
-                articuloSeleccionado.cantidad--;
-                if (articuloSeleccionado.cantidad == 0) {
-                    articuloSeleccionado.cantidad = 1;
-                }
-                localStorage.setItem("fichaArticulo", JSON.stringify(cartItems))
-                renderCarrito(cartsotarge)
-            }
-        }
-
-    });
-
-    eliminar.forEach(button => {
-
-        button.onclick = (art) => {
-            const artid = art.currentTarget.id;
-            const idx = cartItems.findIndex(articulo => articulo.articulo.id == artid)
-
-            if (idx >= 0) {
-                cartItems.splice(idx, 1);
-                localStorage.setItem("fichaArticulo", JSON.stringify(cartItems))
-                renderCarrito(cartsotarge)
-
-                Toastify({
-                    text: "El Producto fue eliminado del carrito Exitosamente!",
-                    duration: 1500,
-                    destination: "",
-                    newWindow: false,
-                    close: false,
-                    gravity: "top", 
-                    position: "center", 
-                    stopOnFocus: true, 
-                    style: {
-                      background: "linear-gradient(to right, #00b09b, #96c93d)",
-                    },
-                    onClick: function(){}
-                  }).showToast();
-            }
-        }
-
-    });
-
-
-vaciarCarro.forEach(button => {
-    button.onclick = async () => { 
+  vaciarCarro.forEach((button) => {
+    button.onclick = async () => {
       const confirmacion = await Swal.fire({
         title: "¿Estás seguro de vaciar el carrito?",
         text: "¡Esta acción no se puede deshacer!",
-        icon: 'warning',
+        icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Si, vaciar',
-        cancelButtonText: 'Cancelar'
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, vaciar",
+        cancelButtonText: "Cancelar",
       });
 
       if (confirmacion.isConfirmed) {
-        cartsotarge = []; 
+        cartsotarge = [];
         localStorage.setItem("fichaArticulo", JSON.stringify(cartsotarge));
-        renderCarrito(cartsotarge); 
-        Swal.fire('Carrito Vacio!', '', 'success'); 
+        renderCarrito(cartsotarge);
+        Swal.fire("Carrito Vacio!", "", "success");
       }
-    }
-  })
-  
-  emitirOrdenDePago.forEach(button => {
+    };
+  });
+
+  emitirOrdenDePago.forEach((button) => {
     button.onclick = async () => {
-
-        const { value: email } = await Swal.fire({
-            title: "Gracias por su compra! " + " \nLe enviaremos la orden para proceder al pago.",
-            input: "email",
-            inputLabel: "Por favor complete su mail en el siguiente recuadro",
-            inputPlaceholder: "...........@.............."
-          });
-          if (email) {
-            Swal.fire(`Por favor dirijase al Centro de Pagos con la orden que le fue enviada al correo : ${email}`);
-            cartsotarge = []; 
-            localStorage.setItem("fichaArticulo", JSON.stringify(cartsotarge));
-            renderCarrito(cartsotarge); 
-          }
+      const { value: email } = await Swal.fire({
+        title:
+          "Gracias por su compra! " +
+          " \nLe enviaremos la orden para proceder al pago.",
+        input: "email",
+        inputLabel: "Por favor complete su mail en el siguiente recuadro",
+        inputPlaceholder: "...........@..............",
+      });
+      if (email) {
+        Swal.fire(
+          `Por favor dirijase al Centro de Pagos con la orden que le fue enviada al correo : ${email}`
+        );
+        cartsotarge = [];
+        localStorage.setItem("fichaArticulo", JSON.stringify(cartsotarge));
+        renderCarrito(cartsotarge);
+      }
+    };
+  });
 }
-})
 
- }
-
-renderCarrito(cartsotarge)
-
-
-
+renderCarrito(cartsotarge);
--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 let articulos = [];
-let fichaArticulo 
-let fichaDeArticuloLS = localStorage.getItem("fichaArticulo")
-if(fichaDeArticuloLS){
-    fichaArticulo = JSON.parse(fichaDeArticuloLS)
-}else {
-    fichaArticulo = []
+let fichaArticulo;
+let fichaDeArticuloLS = localStorage.getItem("fichaArticulo");
+if (fichaDeArticuloLS) {
+  fichaArticulo = JSON.parse(fichaDeArticuloLS);
+} else {
+  fichaArticulo = [];
 }
 
-
-let listaDeArticulos = document.getElementById("articulos")
-
+let listaDeArticulos = document.getElementById("articulos");
 
 function renderProducto(data) {
+  articulos = data;
 
-    articulos = data
-
-    articulos.forEach((articulo) => {
-        const card = document.createElement("div")
-        card.innerHTML = `<nav class="navbar bg-body-tertiary">
+  articulos.forEach((articulo) => {
+    const card = document.createElement("div");
+    card.innerHTML = `<nav class="navbar bg-body-tertiary">
                         <div class="container-fluid"></div>
                         </nav>
                         <div class="container p-3">
@@ -29,118 +26,110 @@ function renderProducto(data) {
                         <button class="boton-aumentar" id="${articulo.articulo.id}">+</button>
                         <button class="cantidad" id="${articulo.articulo.id}">${articulo.cantidad}</button>
                         <button class="boton-disminuir" id="${articulo.articulo.id}">-</button>                      
-                        <button class="articuloAgregar" id="${articulo.articulo.id}">Agregar al Carro</button>` 
-        
-        listaDeArticulos.appendChild (card)                      
-    })
+                        <button class="articuloAgregar" id="${articulo.articulo.id}">Agregar al Carro</button>`;
 
-    botonAgregarAlCarro()
-    configurarCantidades()
+    listaDeArticulos.appendChild(card);
+  });
 
-}                  
+  botonAgregarAlCarro();
+  configurarCantidades();
+}
 
-Swal.fire('Cargando productos...');
+Swal.fire("Cargando productos...");
 Swal.showLoading();
-fetch('./db/data.json').then(resp => {
-    setTimeout(() => {
-        Swal.close();
-        resp.json().then(data => renderProducto(data));
-    }, 2000);
-    
-})
+fetch("./db/data.json").then((resp) => {
+  setTimeout(() => {
+    Swal.close();
+    resp.json().then((data) => renderProducto(data));
+  }, 2000);
+});
 
+function botonAgregarAlCarro() {
+  let addbutton = document.querySelectorAll(".articuloAgregar");
+  console.log(addbutton);
+  addbutton.forEach((button) => {
+    button.onclick = (art) => {
+      try {
+        const artid = art.currentTarget.id;
+        const articuloSeleccionado = articulos.find(
+          (articulo) => articulo.articulo.id == artid
+        );
 
-function botonAgregarAlCarro () {
-    let addbutton = document.querySelectorAll(".articuloAgregar");
-    console.log(addbutton);
-    addbutton.forEach (button => {
-      button.onclick = (art) => {
-        try {
-          const artid = art.currentTarget.id;
-          const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid);
-  
-          const articuloCarrito = fichaArticulo.find(articulo => articulo.articulo.id == artid);
-  
-          if (!articuloCarrito) {
-            fichaArticulo.push(articuloSeleccionado);
-          } else {
-            articuloCarrito.cantidad += articuloSeleccionado.cantidad;
-          }
-  
-          console.log(fichaArticulo);
-          localStorage.setItem("fichaArticulo", JSON.stringify(fichaArticulo));
-  
-          Toastify({
-            text: "El Producto fue añadido al carrito Exitosamente!",
-            duration: 1500,
-            destination: "",
-            newWindow: false,
-            close: false,
-            gravity: "top", 
-            position: "center", 
-            stopOnFocus: true, 
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){}
-          }).showToast();
-  
-        } catch (error) {
-          console.error("Error al agregar producto al carrito:", error);
-          
-        } finally {
-          console.log("Finalizando la función botonAgregarAlCarro");
+        const articuloCarrito = fichaArticulo.find(
+          (articulo) => articulo.articulo.id == artid
+        );
+
+        if (!articuloCarrito) {
+          fichaArticulo.push(articuloSeleccionado);
+        } else {
+          articuloCarrito.cantidad += articuloSeleccionado.cantidad;
         }
+
+        console.log(fichaArticulo);
+        localStorage.setItem("fichaArticulo", JSON.stringify(fichaArticulo));
+
+        Toastify({
+          text: "El Producto fue añadido al carrito Exitosamente!",
+          duration: 1500,
+          destination: "",
+          newWindow: false,
+          close: false,
+          gravity: "top",
+          position: "center",
+          stopOnFocus: true,
+          style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+          onClick: function () {},
+        }).showToast();
+      } catch (error) {
+        console.error("Error al agregar producto al carrito:", error);
+      } finally {
+        console.log("Finalizando la función botonAgregarAlCarro");
       }
-    })
-  }
-
-
+    };
+  });
+}
 
 function configurarCantidades() {
+  let cantidad = document.querySelectorAll(".cantidad");
+  let aumentar = document.querySelectorAll(".boton-aumentar");
+  let disminuir = document.querySelectorAll(".boton-disminuir");
+  let contador = 0;
 
-    let cantidad = document.querySelectorAll(".cantidad")
-    let aumentar = document.querySelectorAll(".boton-aumentar")
-    let disminuir = document.querySelectorAll(".boton-disminuir")
-    let contador = 0
+  aumentar.forEach((button) => {
+    button.onclick = (art) => {
+      const artid = art.currentTarget.id;
+      const articuloSeleccionado = articulos.find(
+        (articulo) => articulo.articulo.id == artid
+      );
+      articuloSeleccionado.cantidad++;
 
-    aumentar.forEach(button => {
+      cantidad.forEach((element) => {
+        if (element.id == artid) {
+          element.innerHTML = articuloSeleccionado.cantidad;
+        }
+      });
+    };
+  });
 
-        button.onclick = (art) => {
-            
-            const artid = art.currentTarget.id;
-            const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid);
-            articuloSeleccionado.cantidad++;
+  disminuir.forEach((button) => {
+    button.onclick = (art) => {
+      const artid = art.currentTarget.id;
+      const articuloSeleccionado = articulos.find(
+        (articulo) => articulo.articulo.id == artid
+      );
+      articuloSeleccionado.cantidad--;
 
-            cantidad.forEach(element => {
-                if (element.id == artid) {
-                    element.innerHTML = articuloSeleccionado.cantidad;
-                }
-            });
+      if (articuloSeleccionado.cantidad < 1) {
+        articuloSeleccionado.cantidad = 1;
+      }
 
-        };
-
-    });
-
-    disminuir.forEach(button => {
-
-        button.onclick = (art) => {
-
-            const artid = art.currentTarget.id;
-            const articuloSeleccionado = articulos.find(articulo => articulo.articulo.id == artid);
-            articuloSeleccionado.cantidad--;
-
-            if (articuloSeleccionado.cantidad < 1) {
-                articuloSeleccionado.cantidad = 1;
-            }
-
-            cantidad.forEach(element => {
-                if (element.id == artid) {
-                    element.innerHTML = articuloSeleccionado.cantidad;
-                }
-            });
-
-        };
-
-    });
+      cantidad.forEach((element) => {
+        if (element.id == artid) {
+          element.innerHTML = articuloSeleccionado.cantidad;
+        }
+      });
+    };
+  });
 }
